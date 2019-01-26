77164cb4b37283a79cd165c1fe39823fb76fc567
valentinmagrez <valentin.magrez@gmail.com>
2019-01-26 21:05:08 +0100

    :lipstick: Add name in admin interface

--- Crawler/Crawler/Models/Entity/UriBetsToParse.cs
+++ Crawler/Crawler/Models/Entity/UriBetsToParse.cs
@@ -1,8 +1,11 @@
-﻿namespace Crawler.Models.Entity
+﻿using System.ComponentModel;
+
+namespace Crawler.Models.Entity
 {
     public class UriBetsToParse
     {
         public int Id { get; set; }
+        [DisplayName("Nom")]
         public string Name { get; set; }
         public string Value { get; set; }
     }
