9afee87d195063a05841858cf1fedfd37ba198f4
valentinmagrez <valentin.magrez@gmail.com>
2019-01-27 19:27:32 +0100

    :sparkles: Add bet endpoint

---
+++ BetService/BetService/Controllers/BetsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BetService.Models;
+
+namespace BetService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BetsController : ControllerBase
+    {
+        private readonly BetDbContext _context;
+
+        public BetsController(BetDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bet>>> GetBetItems()
+        {
+            return await _context.BetItems.ToListAsync();
+        }
+
+        // GET: api/Bets/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bet>> GetBet(long id)
+        {
+            var bet = await _context.BetItems.FindAsync(id);
+
+            if (bet == null)
+            {
+                return NotFound();
+            }
+
+            return bet;
+        }
+
+        // PUT: api/Bets/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBet(long id, Bet bet)
+        {
+            if (id != bet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BetExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bets
+        [HttpPost]
+        public async Task<ActionResult<Bet>> PostBet(Bet bet)
+        {
+            _context.BetItems.Add(bet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBet", new { id = bet.Id }, bet);
+        }
+
+        // DELETE: api/Bets/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Bet>> DeleteBet(long id)
+        {
+            var bet = await _context.BetItems.FindAsync(id);
+            if (bet == null)
+            {
+                return NotFound();
+            }
+
+            _context.BetItems.Remove(bet);
+            await _context.SaveChangesAsync();
+
+            return bet;
+        }
+
+        private bool BetExists(long id)
+        {
+            return _context.BetItems.Any(e => e.Id == id);
+        }
+    }
+}
