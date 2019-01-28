a76b640ee9c043de6ae2763cfdeb98a7de03fb70
valentinmagrez <valentin.magrez@gmail.com>
2019-01-28 20:16:48 +0100

    :heavy_plus_sign: Add swagger for api

--- BetService/BetService/Startup.cs
+++ BetService/BetService/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BetService
 {
@@ -28,6 +29,12 @@ namespace BetService
             var connection = @"Server=db,1433;Database=master;User=sa;Password=password12345PASSWORD;";
             services.AddDbContext<BetDbContext>(
                 options => options.UseSqlServer(connection));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "BetService Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +56,22 @@ namespace BetService
                 app.UseHsts();
             }
 
+            ParametrizeSwagger(app);
+
             app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
 
+        }
+
+        private static void ParametrizeSwagger(IApplicationBuilder app)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "BetService Api V1"); });
         }
     }
 }
